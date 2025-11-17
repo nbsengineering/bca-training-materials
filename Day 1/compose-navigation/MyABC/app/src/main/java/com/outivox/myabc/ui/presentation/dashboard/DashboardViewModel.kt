25367c36d87223752edf77744354cad73cfd65ef
Fachridan Tio <fachridantm@gmail.com>
2025-11-17 15:57:26 +0700

    clean

--- Day 1/compose-navigation/MyABC/app/src/main/java/com/outivox/myabc/ui/presentation/dashboard/DashboardViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.outivox.myabc.ui.presentation.dashboard
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class DashboardViewModel @Inject constructor() : ViewModel()
