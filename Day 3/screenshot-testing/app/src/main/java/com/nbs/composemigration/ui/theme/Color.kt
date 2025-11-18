6c4a568549beac0160f880c53751bdbdfb105f88
abrar <abrar@nusantarabetastudio.com>
2025-11-18 19:46:28 +0700

    add screenshot testing start

---
+++ Day 3/screenshot-testing/app/src/main/java/com/nbs/composemigration/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.nbs.composemigration.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF41054B)
+val DarkPurple = Color(0xFF1E0021)
+val MediumPurple = Color(0xFF28002B)
