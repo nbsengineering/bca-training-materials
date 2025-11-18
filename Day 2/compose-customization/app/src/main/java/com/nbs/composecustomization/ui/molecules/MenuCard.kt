b4c17b24f3eb63a3988f7b135cbb9f00789e5ce1
Fachridan Tio <fachridantm@gmail.com>
2025-11-18 14:55:57 +0700

    add custom modifier

--- Day 2/compose-customization/app/src/main/java/com/nbs/composecustomization/ui/molecules/MenuCard.kt
+++ Day 2/compose-customization/app/src/main/java/com/nbs/composecustomization/ui/molecules/MenuCard.kt
@@ -2,22 +2,17 @@ package com.nbs.composecustomization.ui.molecules
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.nbs.composecustomization.R
+import com.nbs.composecustomization.ui.atoms.SurfaceIcon
 import com.nbs.composecustomization.ui.theme.colorOnPrimaryContainer
 import com.nbs.composecustomization.ui.theme.colorPrimaryContainer
 
@@ -37,14 +32,12 @@ fun MenuCard(attribute: MenuCardAttribute, onClick: (() -> Unit)? = null) {
             colors = CardDefaults.cardColors(containerColor = colorPrimaryContainer),
             onClick = { onClick?.invoke() }
         ) {
-            Box(modifier = Modifier.padding(16.dp)) {
-                Icon(
-                    modifier = Modifier.size(20.dp),
-                    painter = painterResource(attribute.iconRes),
-                    contentDescription = null,
-                    tint = colorOnPrimaryContainer,
-                )
-            }
+            SurfaceIcon(
+                size = 20.dp,
+                iconRes = attribute.iconRes,
+                backgroundColor = colorPrimaryContainer,
+                iconColor =  colorOnPrimaryContainer,
+            )
         }
 
         Text(text = attribute.label)
